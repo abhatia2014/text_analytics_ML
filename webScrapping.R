4912a017c3a31d09b3b5265c87e1064af95ac016
Aankur Bhatia <aank2007@gmail.com>
2016-10-01 14:51:47 -0400

    will start from here

---
+++ webScrapping.R
@@ -0,0 +1,13 @@
+#we'll be looking at reviews for 5 most popular romance books
+
+#Part 1 - Webscrapping, Part 2- Exploratory analysis, Part 3- Machine Learning
+
+#we'll be loading the RSelenium package 
+
+
+# Load Required Libararies ------------------------------------------------
+
+library(data.table)
+library(dplyr)
+library(magrittr)
+library(rvest)
