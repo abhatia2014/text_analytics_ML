10f026f4ea2ea43d17e2d5127630f661eeac2b47
Aankur Bhatia <aank2007@gmail.com>
2016-10-12 21:00:23 -0400

    another commit try

--- webScrapping.R
+++ webScrapping.R
@@ -1,5 +1,5 @@
 #we'll be looking at reviews for 5 most popular romance books
-
+#Let's try and commit once
 #Part 1 - Webscrapping, Part 2- Exploratory analysis, Part 3- Machine Learning
 
 #we'll be loading the RSelenium package 
@@ -16,8 +16,15 @@ url <- "https://www.goodreads.com/book/show/18619684-the-time-traveler-s-wife#ot
 output.filename="GR_TimeTravelersWife.csv"
 
 #start Selenium Server
-checkForServer()
-startServer()
-remDr=remoteDriver(browserName="safari")
+checkForServer(update = TRUE)
+path="/Users/aankurbhatia/Applications/Firefox.app"
+
+startServer(-Dwebdriver.firefox.bin=pathtoexecutable , log = FALSE, invisible = FALSE)
+
+remDr <- remoteDriver(browser='firefox')
+remDr$checkStatus()
+
+Sys.sleep(10)
 remDr$open()
 remDr$navigate(url)
+
