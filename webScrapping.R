2b0f3290e96a65a0efc44d99968f05e95f578f73
Aankur Bhatia <aank2007@gmail.com>
2016-10-18 22:58:33 -0400

    committing the first web scrapping done successully

--- webScrapping.R
+++ webScrapping.R
@@ -28,3 +28,60 @@ Sys.sleep(10)
 remDr$open()
 remDr$navigate(url)
 
+
+
+# Webscrapping using R ----------------------------------------------------
+
+#Here'll we'll use R's new package rvest to extract addresses from web pages
+#and ggmap to geocode those addresses
+
+#install the leaflet package from github
+
+devtools::install_github("rstudio/leaflet")
+
+library(dplyr)
+library(rvest)
+library(ggmap)
+library(RColorBrewer)
+
+#we'll look for the addresses of Ithica wineries and breweries
+# http://www.visitithaca.com/attractions/wineries.html
+
+url=read_html("http://www.visitithaca.com/attractions/wineries.html")
+
+#find the exact pieces of the website that you need
+
+#The function html_nodes pulls out the entire node from the DOM and then the function html_text allows us to limit to the text within the node. 
+selector_name=".indSearchListingTitle"
+#getting the names of all wineries from the page
+
+frames=html_nodes(x=url,css=selector_name)%>%
+  html_text()
+
+frames
+
+#now extract the addresses
+
+selector_address=".indSearchListingMetaContainer div:nth-child(2) div:nth-child(1) .indMetaInfoWrapper"
+
+faddresses=html_nodes(url,selector_address)%>%
+  html_text()
+
+head(faddresses)
+
+#let's do a little of string cleanup
+
+to_remove=paste(c("\n","^\\s+|\\s+$"),collapse = "|")
+to_remove
+faddresses=gsub(to_remove,"",faddresses)
+
+#now use the ggmap to geocode the addresses
+#first we get the lat, long of all the addresses
+
+geocodes=geocode(faddresses,output = "latlona")
+
+#geocode text cleanup
+
+head(geocodes)
+
+full=mutate(geocodes,names=f)
